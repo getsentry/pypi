5cfb47ca59fd380ffd5f32672c4b8a1b65792b98
anthony sottile <anthony.sottile@sentry.io>
2022-07-22 12:06:03 -0400

    use dumb-pypi 1.10+ packages.json endpoint

--- tests/build_test.py
+++ tests/build_test.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import email.message
-import http
 import io
 import os
 import shutil
@@ -17,9 +15,6 @@ from packaging.version import Version
 import build
 from build import Package
 from build import Wheel
-from testing.resources import resource
-
-get_wheels_uncached = build._get_wheels.__wrapped__
 
 
 def test_supported_tags_does_not_include_generic_linux():
@@ -89,7 +84,7 @@ LINUX_3_8_SUPPORTED_TAGS = frozenset(
 )
 def test_package_satisfied_by_matches(filename):
     package = Package.make("my-pkg==1.2.3", {})
-    wheel = Wheel(filename, "")
+    wheel = Wheel(filename)
     assert package.satisfied_by((wheel,), LINUX_3_8_SUPPORTED_TAGS) is wheel
 
 
@@ -105,71 +100,25 @@ def test_package_satisfied_by_matches(filename):
 )
 def test_package_satisfied_by_does_not_match(filename):
     package = Package.make("my-pkg==1.2.3", {})
-    wheels = (Wheel(filename, ""),)
+    wheels = (Wheel(filename),)
     assert package.satisfied_by(wheels, LINUX_3_8_SUPPORTED_TAGS) is None
 
 
-def test_get_wheels_public_pypi_smoke():
-    bio = io.BytesIO(resource("public-pypi.json"))
+def test_get_internal_wheels():
+    contents = b"""\
+{"filename": "detect_test_pollution-1.0.0-py2.py3-none-any.whl"}
+{"filename": "detect_test_pollution-1.1.0-py2.py3-none-any.whl"}
+{"filename": "detect_test_pollution-1.1.1-py2.py3-none-any.whl"}
+"""
+    bio = io.BytesIO(contents)
     with mock.patch.object(urllib.request, "urlopen", return_value=bio):
-        ret = get_wheels_uncached("https://pypi.org", "detect-test-pollution")
+        ret = build._internal_wheels("https://example.com")
 
     assert ret == (
-        Wheel(
-            filename="detect_test_pollution-1.0.0-py2.py3-none-any.whl",
-            download_url="https://files.pythonhosted.org/packages/c7/48/2124c22bda61648dae5b04e7b9efa86572b94c1c29fd486fb784f3723a99/detect_test_pollution-1.0.0-py2.py3-none-any.whl",
-        ),
-        Wheel(
-            filename="detect_test_pollution-1.1.0-py2.py3-none-any.whl",
-            download_url="https://files.pythonhosted.org/packages/1c/2c/4eb7f928866364c5ae9f1afa59992e87f847c5bbe9c0ace40bdd3bbf49ac/detect_test_pollution-1.1.0-py2.py3-none-any.whl",
-        ),
-        Wheel(
-            filename="detect_test_pollution-1.1.1-py2.py3-none-any.whl",
-            download_url="https://files.pythonhosted.org/packages/e6/aa/76668aa85cb7d811eca9cc9c70abf88dea068782b3602eb58dbe87f51c9d/detect_test_pollution-1.1.1-py2.py3-none-any.whl",
-        ),
+        Wheel(filename="detect_test_pollution-1.0.0-py2.py3-none-any.whl"),
+        Wheel(filename="detect_test_pollution-1.1.0-py2.py3-none-any.whl"),
+        Wheel(filename="detect_test_pollution-1.1.1-py2.py3-none-any.whl"),
     )
-
-
-def test_get_wheels_dumb_pypi_smoke():
-    bio = io.BytesIO(resource("dumb-pypi.json"))
-    with mock.patch.object(urllib.request, "urlopen", return_value=bio):
-        ret = get_wheels_uncached("http://localhost:8000/prefix", "cfgv")
-
-    assert ret == (
-        Wheel(
-            filename="cfgv-3.3.1-py2.py3-none-any.whl",
-            download_url="http://localhost:8000/wheels/cfgv-3.3.1-py2.py3-none-any.whl",
-        ),
-    )
-
-
-def test_get_wheels_404_returns_empty():
-    error = urllib.error.HTTPError(
-        "http://localhost:8000/prefix/simplejson/json",
-        404,
-        http.HTTPStatus(404).phrase,
-        email.message.Message(),
-        io.BytesIO(),
-    )
-    with mock.patch.object(urllib.request, "urlopen", side_effect=error):
-        ret = get_wheels_uncached("http://localhost:8000/prefix", "simplejson")
-
-    assert ret == ()
-
-
-def test_get_dist_does_not_catch_unknown_errors():
-    error = urllib.error.HTTPError(
-        "http://localhost:8000/prefix/simplejson/json",
-        500,
-        http.HTTPStatus(500).phrase,
-        email.message.Message(),
-        io.BytesIO(b"internal server error!"),
-    )
-    with mock.patch.object(urllib.request, "urlopen", side_effect=error):
-        with pytest.raises(urllib.error.HTTPError) as excinfo:
-            get_wheels_uncached("http://localhost:8000/prefix", "simplejson")
-
-    assert excinfo.value == error
 
 
 def test_brew_paths():
