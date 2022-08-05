1c0b192388bb3af047002b4e7aca5bef6eb6a1df
anthony sottile <anthony.sottile@sentry.io>
2022-08-05 11:29:27 -0400

    add helper for adding packages and add pre-commit

---
+++ add_pkg.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import argparse
+import configparser
+import os
+import subprocess
+import sys
+import tempfile
+from typing import Sequence
+
+from packaging.utils import parse_wheel_filename
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dep")
+    parser.add_argument("--packages-ini", default="packages.ini")
+    args = parser.parse_args(argv)
+
+    print(f"resolving {args.dep}...")
+    with tempfile.TemporaryDirectory() as tmpdir:
+        subprocess.check_call(
+            (
+                sys.executable,
+                "-mpip",
+                "wheel",
+                "--quiet",
+                "--no-cache-dir",
+                f"--wheel-dir={tmpdir}",
+                args.dep,
+            )
+        )
+
+        deps = []
+        for filename in sorted(os.listdir(tmpdir)):
+            name, version, _, _ = parse_wheel_filename(filename)
+            deps.append((name, str(version)))
+
+    cfg = configparser.ConfigParser()
+    assert cfg.read(args.packages_ini)
+
+    for name, version in deps:
+        key = f"{name}=={version}"
+        if key in cfg:
+            print(f"{key}: already present!")
+        else:
+            print(f"{key}: adding...")
+
+        # find the best candidate to copy config from
+        copy_from = {}
+        for k, v in cfg.items():
+            if k.startswith(f"{name}=="):
+                copy_from = dict(v)
+        # setting is only useful for 1 version
+        copy_from.pop("ignore_wheels", None)
+
+        cfg[key] = copy_from
+
+    with open(args.packages_ini, "w") as f:
+        cfg.write(f)
+
+    subprocess.call((sys.executable, "-m", "format_ini", args.packages_ini))
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
---
+++ tests/add_pkg_test.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import subprocess
+import tempfile
+from unittest import mock
+
+import pytest
+
+import add_pkg
+
+
+@pytest.fixture
+def pretend_pip(tmp_path):
+    tmp = tmp_path.joinpath("tmp_path")
+    tmp.mkdir()
+
+    with mock.patch.object(tempfile, "TemporaryDirectory") as tmpdir_mck:
+        tmpdir_mck.return_value.__enter__.return_value = tmp
+        with mock.patch.object(subprocess, "check_call"):
+            yield tmp
+
+
+def test_pkg_already_present(pretend_pip, tmp_path, capsys):
+    pretend_pip.joinpath("a-1-py3-none-any.whl").touch()
+
+    packages_ini = tmp_path.joinpath("packages.ini")
+    packages_ini.write_text("[a==1]\n")
+
+    assert add_pkg.main(("a", f"--packages-ini={packages_ini}")) == 0
+
+    assert packages_ini.read_text() == "[a==1]\n"
+
+    out, _ = capsys.readouterr()
+    assert out == "resolving a...\na==1: already present!\n"
+
+
+def test_pkg_copied_from_previous_version(pretend_pip, tmp_path, capsys):
+    pretend_pip.joinpath("a-2-py3-none-any.whl").touch()
+
+    packages_ini = tmp_path.joinpath("packages.ini")
+    packages_ini.write_text(
+        "[a==1]\n"
+        "apt_packages = zlib1g-dev\n"
+        # this will not get copied because it is only revelant to this version
+        "ignore_wheels = a-1-py2.py3-none-any.whl\n"
+    )
+
+    assert add_pkg.main(("a", f"--packages-ini={packages_ini}")) == 0
+
+    assert packages_ini.read_text() == (
+        "[a==1]\n"
+        "apt_packages = zlib1g-dev\n"
+        "ignore_wheels = a-1-py2.py3-none-any.whl\n"
+        "[a==2]\n"
+        "apt_packages = zlib1g-dev\n"
+    )
+
+    out, _ = capsys.readouterr()
+    assert out == "resolving a...\na==2: adding...\n"
