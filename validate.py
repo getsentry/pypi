095adb235daeb8ce60f70aa9e63991695b6a565c
anthony sottile <anthony.sottile@sentry.io>
2024-02-05 16:27:51 -0500

    build packages for python 3.12

--- validate.py
+++ validate.py
@@ -15,7 +15,7 @@ from packaging.tags import Tag
 from packaging.utils import parse_wheel_filename
 from packaging.version import Version
 
-PYTHONS = ((3, 10), (3, 11))
+PYTHONS = ((3, 10), (3, 11), (3, 12))
 DIST_INFO_RE = re.compile(r"^[^/]+.dist-info/[^/]+$")
 
 
