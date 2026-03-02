cf10eb11c9406c3ea2fdf017e65a2bac17102e23
Joshua Li <joshuarli98@gmail.com>
2026-03-02 10:59:36 -0800

    pc

--- validate.py
+++ validate.py
@@ -181,7 +181,9 @@ def main() -> int:
                 )
             except subprocess.CalledProcessError:
                 if python == "python3.14":
-                    print(f"!!! WARNING (py3.14) validation: {name}=={version} ({python})")
+                    print(
+                        f"!!! WARNING (py3.14) validation: {name}=={version} ({python})"
+                    )
                 else:
                     failed.append(f"{name}=={version} ({python})")
                     print(f"!!! FAILED validation: {name}=={version} ({python})")
